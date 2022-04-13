3a8ea61ebeff7d454e1e535f5f68c5878ae1f710
andrew.ibrahim <andrew.ibrahim@robovics.com>
2022-04-13 17:44:43 +0200

    first commit

---
+++ resources/views/dashboard/includes/header.blade.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<title>{{ config('app.name', 'POS') }}</title>
+
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="dist/css/adminlte.min.css">
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+
+
+
